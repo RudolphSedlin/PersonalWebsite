f2e6bf0e69143c57e39bbf127862b57a7c12edc8
RudolphSedlin <Rudolph.Sedlin@protonmail.com>
2025-02-24 15:59:19 -0500

    Website update.

--- src/assets/Projects.js
+++ src/assets/Projects.js
@@ -1,5 +1,13 @@
 const PROJECTS = [
     {
+        title: 'UptimesElectric',
+        description:  'A small project written in C++ that evaluated the uptimes of an electric vehicle charging network comprising of a number of stations and chargers that are up only at various times.',
+        time: '01/2025',
+        links: [],
+        images: [],
+    },
+
+    {
         title: 'VitaTrack',
         description:  'VitaTrack is a medical software for analyzing the contents of foodstuffs using LLMs and image recognition. Simply take a photograph, and have all the relevant food data anaylzed and added for your reflection. It also supports the ability to see cumulative food statistics, get alternative food suggestions, and provide general dietary goals.',
         role: 'Operated as Backend Lead, Project Lead, and Scrum Master. Managed the overall project architecture. Developed and remotely deployed a secure and robust backend and CI/CD pipeline; the latter of which activates on push to Github and sets up a fresh testing environment, runs assosciated tests, and automatically merges and deploys if successful. Contributed to the front-end in select-places.',
