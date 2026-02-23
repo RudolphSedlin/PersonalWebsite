6fe61cad7a27959b19f8fab86edb49b2082433c4
RudolphSedlin <Rudolph.Sedlin@protonmail.com>
2026-02-22 22:26:49 -0500

    Website update.

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,42 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
 
-import {Route, Routes, Navigate} from 'react-router-dom'
+import { Route, Routes, Navigate, useLocation } from "react-router-dom";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
-import About from './components/About.jsx'
-import Certifications from './components/Certifications.jsx'
-import Projects from './components/Projects.jsx'
-import Blog from './components/Blog.jsx'
-import Contact from './components/Contact.jsx'
+import About from "./components/About.jsx";
+import Certifications from "./components/Certifications.jsx";
+import Projects from "./components/Projects.jsx";
+import Blog from "./components/Blog.jsx";
+import Contact from "./components/Contact.jsx";
 
 function App() {
+  const location = useLocation();
+
   return (
-    <div className='App'>
-        <Routes>
-          <Route path='/' element={<About />} />
-          <Route path='/Certifications' element={<Certifications />} />
-          <Route path='/Projects' element={<Projects />} />
-          <Route path='/Blog' element={<Blog />} />
-          <Route path='/Contact' element={<Contact />} />
-          <Route
-            path="*"
-            element={<Navigate to="/" replace />}
-          />
-        </Routes>
-      </div>
+    <div className="App">
+    <TransitionGroup component={null}>
+    <CSSTransition
+    key={location.pathname}
+    classNames="page"
+    timeout={250}
+    unmountOnExit
+    >
+    <div className="page-wrap">
+    {/* Pass location so the transition uses the keyed location */}
+    <Routes location={location}>
+    <Route path="/" element={<About />} />
+    <Route path="/Certifications" element={<Certifications />} />
+    <Route path="/Projects" element={<Projects />} />
+    <Route path="/Blog" element={<Blog />} />
+    <Route path="/Contact" element={<Contact />} />
+    <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+    </div>
+    </CSSTransition>
+    </TransitionGroup>
+    </div>
   );
 }
 
-export default App
+export default App;
